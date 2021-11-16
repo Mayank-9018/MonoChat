5bb1cd385a744480b9ef68eb06783defbbf4ef14
Mayank Gupta <ubuntu568@gmail.com>
2021-11-16 21:21:24 +0530

    Wrap MaterialApp inside MultiProvider
    and add MessageDao() provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:monochat/components/message_list.dart';
+import 'package:monochat/models/message_dao.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // TODO: Firebase initialization here
+  await Firebase.initializeApp();
+  // TODO: Check if using await is right here or not
   runApp(const App());
 }
 
@@ -12,9 +16,17 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'MonoChat',
-      home: MessageList(),
+    return MultiProvider(
+      providers: [
+        Provider<MessageDao>(
+          lazy: false,
+          create: (__) => MessageDao(),
+        )
+      ],
+      child: const MaterialApp(
+        title: 'MonoChat',
+        home: MessageList(),
+      ),
     );
   }
 }
