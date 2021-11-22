3f357fe3287f3ce90c421e7ab162b8cc3fbe242e
Mayank Gupta <ubuntu568@gmail.com>
2021-11-22 22:26:48 +0530

    Add a message input box at the bottom

--- lib/components/message_list.dart
+++ lib/components/message_list.dart
@@ -8,13 +8,58 @@ class MessageList extends StatefulWidget {
 }
 
 class _MessageListState extends State<MessageList> {
+  final TextEditingController _messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('MonoChat'),
       ),
-      body: const Placeholder(),
+      bottomSheet: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: TextField(
+          textAlignVertical: TextAlignVertical.top,
+          minLines: 1,
+          maxLines: 6,
+          keyboardType: TextInputType.text,
+          controller: _messageController,
+          onSubmitted: (input) {
+            // TODO: Add Message DAO 1
+            _sendMessage();
+          },
+          decoration: InputDecoration(
+              suffixIcon: InkWell(
+                borderRadius: const BorderRadius.all(Radius.circular(25.0)),
+                child: const Icon(Icons.send),
+                onTap: () {},
+              ),
+              border: const UnderlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.all(Radius.circular(25.0))),
+              filled: true,
+              hintText: 'Enter new message'),
+        ),
+      ),
+      body: Column(
+        children: [
+          _getMessageList(),
+        ],
+      ),
     );
+  }
+
+  bool _canSendMessage() => _messageController.text.isNotEmpty;
+
+  void _sendMessage() {}
+
+  Widget _getMessageList() {
+    return const SizedBox.shrink();
   }
 }
