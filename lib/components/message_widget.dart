18b673a4052ba39174faca976261885375e897db
Mayank Gupta <ubuntu568@gmail.com>
2021-11-23 19:59:56 +0530

    Show time on messages

--- lib/components/message_widget.dart
+++ lib/components/message_widget.dart
@@ -16,35 +16,48 @@ class MessageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        CircleAvatar(
-          backgroundColor: [
-            Colors.red,
-            Colors.green,
-            Colors.blue,
-            Colors.yellow,
-            Colors.white
-          ].elementAt(Random().nextInt(5)),
-        ),
-        const SizedBox(
-          width: 5,
-        ),
-        ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 250),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).cardColor,
-              borderRadius: const BorderRadius.all(
-                Radius.circular(15),
+        Row(
+          children: [
+            CircleAvatar(
+              backgroundColor: [
+                Colors.red,
+                Colors.green,
+                Colors.blue,
+                Colors.yellow,
+                Colors.white
+              ].elementAt(Random().nextInt(5)),
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 250),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).cardColor,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(message),
+                ),
               ),
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Text(message),
-            ),
-          ),
+          ],
         ),
+        Padding(
+          padding: const EdgeInsets.only(left: 55),
+          child: Text(
+            //TODO: Show Date and Day for older messages
+            DateFormat('h:mm a').format(date).toString(),
+            style: const TextStyle(color: Colors.grey),
+          ),
+        )
       ],
     );
   }
