79ad9e027e1ca184ca55bb27f1a9f9dddbd05066
Mayank Gupta <ubuntu568@gmail.com>
2021-11-28 16:26:26 +0530

    Fetch data from 'users' collection to display on UserScreen

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -3,6 +3,7 @@ import 'package:intl/intl.dart';
 import 'package:monochat/components/logout_dialog.dart';
 import 'package:monochat/components/user_image.dart';
 import 'package:monochat/models/current_user_dao.dart';
+import 'package:monochat/models/user_dao.dart';
 import 'package:provider/provider.dart';
 
 class UserScreen extends StatefulWidget {
@@ -14,70 +15,83 @@ class UserScreen extends StatefulWidget {
 
 class _UserScreenState extends State<UserScreen> {
   late final CurrentUserDao currentUserDao;
+
   @override
   void initState() {
     super.initState();
     currentUserDao = Provider.of<CurrentUserDao>(context, listen: false);
   }
 
+  Future<Map<String, dynamic>> getUserData() async {
+    return await Provider.of<UserDao>(context, listen: false)
+        .getUserData(currentUserDao.userId()!);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final Future<Map<String, dynamic>> userData = getUserData();
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () => showDialog(
-                context: context, builder: (con) => const LogoutDialog()),
-            icon: const Icon(Icons.logout),
-            tooltip: 'Logout',
-          )
-        ],
-      ),
-      body: ListView(
-        physics: const BouncingScrollPhysics(),
-        children: [
-          const SizedBox(
-            height: 40,
-          ),
-          Hero(
-              tag: currentUserDao.userId()!,
-              child: UserImage(
-                currentUserDao.photoUrl(),
-                radius: 100,
-              )),
-          const SizedBox(
-            height: 15,
-          ),
-          TextFormField(
-            textAlign: TextAlign.center,
-            initialValue:
-                (currentUserDao.name() ?? "").isNotEmpty ? currentUserDao.name()! : 'No Name',
-            style: Theme.of(context).textTheme.headline4,
-            decoration: const InputDecoration(border: InputBorder.none),
-            onFieldSubmitted: (text) => currentUserDao.updateName(text),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          ListTile(
-            leading: const Icon(Icons.email_outlined),
-            title: Text(currentUserDao.email()!),
-            subtitle: const Text('Email address'),
-            trailing: const Tooltip(
-              message: 'Email Verified!',
-              child: Icon(
-                Icons.verified,
-                color: Colors.green,
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () => showDialog(
+                  context: context, builder: (con) => const LogoutDialog()),
+              icon: const Icon(Icons.logout),
+              tooltip: 'Logout',
+            )
+          ],
+        ),
+        body: FutureBuilder<Map<String, dynamic>>(
+          initialData: const <String, dynamic>{},
+          future: userData,
+          builder: (cont, snap) => ListView(
+            physics: const BouncingScrollPhysics(),
+            children: [
+              const SizedBox(
+                height: 40,
               ),
-            ),
+              Hero(
+                  tag: currentUserDao.userId()!,
+                  child: UserImage(
+                    currentUserDao.userId()!,
+                    radius: 100,
+                  )),
+              const SizedBox(
+                height: 15,
+              ),
+              TextFormField(
+                textAlign: TextAlign.center,
+                initialValue:
+                    (snap.data?['name'] ?? "").isNotEmpty ? null : 'No Name',
+                style: Theme.of(context).textTheme.headline4,
+                decoration: const InputDecoration(border: InputBorder.none),
+                onFieldSubmitted: (text) {},
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ListTile(
+                leading: const Icon(Icons.email_outlined),
+                title: Text(snap.data?['email'] ?? ''),
+                subtitle: const Text('Email address'),
+                trailing: const Tooltip(
+                  message: 'Email Verified!',
+                  child: Icon(
+                    Icons.verified,
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.calendar_today_outlined),
+                title: Text(snap.data!.isNotEmpty
+                    ? DateFormat('d LLLL y')
+                        .format(snap.data?['creationDate'].toDate())
+                    : ''),
+                subtitle: const Text('Account creation date'),
+              ),
+            ],
           ),
-          ListTile(
-            leading: const Icon(Icons.calendar_today_outlined),
-            title: Text(DateFormat('d LLLL y').format(currentUserDao.creationDate()!)),
-            subtitle: const Text('Account creation date'),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
