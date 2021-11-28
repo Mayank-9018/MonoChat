5a86d9f285ca13d01df0057b78445a5b8275ef98
Mayank Gupta <ubuntu568@gmail.com>
2021-11-28 14:37:55 +0530

    Fetch user's name and photoUrl from 'users' collection to show in MessageWidget

--- lib/components/message_widget.dart
+++ lib/components/message_widget.dart
@@ -1,7 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
+import 'package:monochat/models/user_dao.dart';
 
 extension DateOnlyCompare on DateTime {
   bool isSameDate(DateTime other) {
@@ -13,29 +13,39 @@ class MessageWidget extends StatelessWidget {
   final String message;
   final DateTime date;
   final String uid;
-  final bool fromUser;
 
-  const MessageWidget(this.message, this.date, this.uid,
-      {Key? key, this.fromUser = false})
-      : super(key: key);
+  const MessageWidget(
+    this.message,
+    this.date,
+    this.uid, {
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Future<Map<String, dynamic>> userDataFuture = getUserData(context);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            CircleAvatar(
-              backgroundColor: [
-                Colors.red,
-                Colors.green,
-                Colors.blue,
-                Colors.yellow,
-                Colors.white
-              ].elementAt(Random().nextInt(5)),
-            ),
+            FutureBuilder<Map<String, dynamic>>(
+                future: userDataFuture,
+                builder: (con, snapshot) {
+                  return CircleAvatar(
+                    backgroundColor: Colors.grey[200],
+                    child: snapshot.data?['photoUrl'] == null
+                        ? const Icon(
+                            Icons.person,
+                            color: Colors.black,
+                          )
+                        : null,
+                    foregroundImage: snapshot.data?['photoUrl'] == null
+                        ? null
+                        : NetworkImage(snapshot.data?['photoUrl']),
+                  );
+                }),
             const SizedBox(
               width: 5,
             ),
@@ -57,18 +67,39 @@ class MessageWidget extends StatelessWidget {
           ],
         ),
         Padding(
-          padding: const EdgeInsets.only(left: 55),
-          child: Text(
-            DateFormat(date.isSameDate(DateTime.now())
-                        ? 'h:mm a'
-                        : 'LLL d • h:mm a')
-                    .format(date)
-                    .toString() +
-                " • $uid",
-            style: const TextStyle(color: Colors.grey, fontSize: 12),
-          ),
-        )
+            padding: const EdgeInsets.only(left: 55),
+            child: FutureBuilder<Map<String, dynamic>>(
+                future: userDataFuture,
+                builder: (cont, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(
+                        DateFormat(date.isSameDate(DateTime.now())
+                                    ? 'h:mm a'
+                                    : 'LLL d • h:mm a')
+                                .format(date)
+                                .toString() +
+                            " • " +
+                            (snapshot.data!['name'] ?? snapshot.data!['email']),
+                        style:
+                            const TextStyle(color: Colors.grey, fontSize: 12));
+                  } else {
+                    return Text(
+                        DateFormat(date.isSameDate(DateTime.now())
+                                ? 'h:mm a'
+                                : 'LLL d • h:mm a')
+                            .format(date)
+                            .toString(),
+                        style:
+                            const TextStyle(color: Colors.grey, fontSize: 12));
+                  }
+                }))
       ],
     );
+  }
+
+  Future<Map<String, dynamic>> getUserData(BuildContext context) async {
+    var query =
+        await Provider.of<UserDao>(context, listen: false).getUserDate(uid);
+    return query.docs.first.data() as Map<String, dynamic>;
   }
 }
